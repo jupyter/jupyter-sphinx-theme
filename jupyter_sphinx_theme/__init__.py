b387c457d8d7a5b8e9e7515f566934650959624c
Jonathan Frederic <jon.freder@gmail.com>
2016-03-23 16:16:27 -0700

    Adjust installation instructions to load conf.py settings

--- jupyter_sphinx_theme/__init__.py
+++ jupyter_sphinx_theme/__init__.py
@@ -1,5 +1,6 @@
 """Jupyter sphinx theme."""
 import os
+from recommonmark.parser import CommonMarkParser
 
 VERSION = (0, 0, 1)
 
@@ -10,3 +11,28 @@ def get_html_theme_path():
     """Return list of HTML theme paths."""
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     return [cur_dir]
+
+# Conf.py import settings
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+html_theme = 'jupyter'
+html_theme_path = get_html_theme_path()
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'nbsphinx'
+]
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.md', '.ipynb']
+
+html_sidebars = {
+    '**': [
+        'sidebartoc.html'
+    ]
+}
